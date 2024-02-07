88fc3a127a07b63e12492e8704fa32a3d1d55dac
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-02-07 23:02:10 +0300

    My first kotlin assignment

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main(){fun main(){
+    var name = "margaretNyambura"
+    println(name)
+    var age: Byte = 21
+    println(age)
+    var phoneNumber = +254798657493
+    println(phoneNumber)
+    var weightInKg: Float = 65.6F
+    println(weightInKg)
+    var citizenOfKenya: Boolean = true
+    println(citizenOfKenya)
+    var nonCitizenOfKenya: Boolean = false
+    println(nonCitizenOfKenya)
+
+}
+
+
+
+}
